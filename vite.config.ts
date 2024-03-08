b7c32238f61abbcc5661a2fb268c10d406e3115c
Chandan Kumar <chandankumar65536@gmail.com>
2024-03-08 16:18:20 +0530

    github deploy commit

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,7 @@ import { resolve } from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+    base: "/webapp/",
     assetsInclude: ['**/*.gltf','**/*.bin', '**/*.glsl', '**/*.png'],
     build: {
         rollupOptions: {
